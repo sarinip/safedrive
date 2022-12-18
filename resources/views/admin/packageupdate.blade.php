cafcdb03e14fecb259d440346a6291043619e7c2
Avishka Rathnayake <123456789ymail@gmail.com>
2022-12-18 23:52:13 +0530

    package changes - Avishka

--- resources/views/admin/packageupdate.blade.php
+++ resources/views/admin/packageupdate.blade.php
@@ -18,6 +18,9 @@
                         <div class="form-sample">
                             <div class="row">
                                 <div class="col-md-6">
+
+
+
                                     <div class="form-group row">
                                         <label class="col-sm-3 col-form-label">Package Name</label>
                                         <div class="col-sm-9">
